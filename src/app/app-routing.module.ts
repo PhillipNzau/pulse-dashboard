00017a55a8aacf1e9e1d9f648d18cdf85c195d4b
phillipnzau <nzauonline@gmail.com>
2023-07-25 20:16:32 +0300

    login ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  {path: '', loadChildren:() => import('./dashboard/dashboard.routes').then(mod => mod.DASHBOARD_ROUTES)}
+  {
+    path: 'home', loadChildren:() => import('./dashboard/dashboard.routes').then(mod => mod.DASHBOARD_ROUTES)
+  },
+  {
+    path: '', component: LoginComponent
+  }
+
 ];
 
 @NgModule({
