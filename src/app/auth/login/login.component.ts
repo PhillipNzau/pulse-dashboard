061339d736dc80981c6bdfd51ee7c60f4e958256
phillipnzau <nzauonline@gmail.com>
2023-07-25 20:24:15 +0300

    login ui cont

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../auth-service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(4)]]
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+  }
+
+  // login function
+  loginUser() {
+    const loginData = {
+      ...this.loginForm.value
+    }
+    // this.authService.add(loginData).subscribe({
+    //   next: (res:any) => { 
+    //   },
+    //   error:(err:any) => { console.log('Login Data Err', err);
+    //   }
+    // });
+  }
 
 }
