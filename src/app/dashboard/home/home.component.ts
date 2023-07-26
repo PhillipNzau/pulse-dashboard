8373a3108b15036514fa23b90d25b367dca48857
phillipnzau <nzauonline@gmail.com>
2023-07-26 16:20:29 +0300

    Dashboard cont

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemCardComponent } from "../shared/item-card/item-card.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [CommonModule, ItemCardComponent]
 })
 export class HomeComponent {
 
