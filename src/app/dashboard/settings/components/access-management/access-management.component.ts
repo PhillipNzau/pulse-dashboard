220a4ce73402a72b38390703639a2c4746b6aae5
phillipnzau <nzauonline@gmail.com>
2023-07-31 13:02:18 +0300

    modal init

--- src/app/dashboard/settings/components/access-management/access-management.component.ts
+++ src/app/dashboard/settings/components/access-management/access-management.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DeleteModalComponent } from "../../../shared/components/modal/modal.component";
 
 @Component({
-  selector: 'app-access-management',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './access-management.component.html',
-  styleUrls: ['./access-management.component.scss']
+    selector: 'app-access-management',
+    standalone: true,
+    templateUrl: './access-management.component.html',
+    styleUrls: ['./access-management.component.scss'],
+    imports: [CommonModule, DeleteModalComponent]
 })
 export class AccessManagementComponent {
   isEditUser:boolean = false;
   isAddUser:boolean = false;
+  showModal:boolean = false;
+
+  closeModal(): void {
+    console.log('sdsd');
+    
+    this.showModal = false;
+  }
 }
