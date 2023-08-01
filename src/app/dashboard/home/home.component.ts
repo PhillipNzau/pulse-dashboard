3d168573473218519ec0e37bc3ff0c99e4f1d9d7
phillipnzau <nzauonline@gmail.com>
2023-08-01 20:41:54 +0300

    date picker init

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -5,6 +5,7 @@ import { Chart, ChartModule } from 'angular-highcharts';
 import { DropdownComponent } from "../shared/components/dropdown/dropdown.component";
 import { DropdownOption } from '../shared/models/dropdownModel';
 import { DateRangePickerComponent } from "../shared/components/date-range-picker/date-range-picker.component";
+import * as dayjs from 'dayjs'
 
 @Component({
     selector: 'app-home',
@@ -43,13 +44,14 @@ export class HomeComponent {
       });
 
       dropdownOptions: DropdownOption[] = [
-        { label: 'Today', value: 1 },
-        { label: 'This Week', value: 2 },
-        { label: 'Last Week', value: 3 },
-        { label: 'This Month', value: 3 },
-        { label: 'Last Month', value: 3 },
-        { label: 'This Year', value: 3 },
+        { label: 'Today', value: [dayjs(), dayjs()] },
+        { label: 'Yesterday', value: [dayjs().subtract(1, 'days'), dayjs().subtract(1, 'days')] },
+        { label: 'Last 7 Days', value: [dayjs().subtract(6, 'days'), dayjs()] },
+        { label: 'Last 30 Days', value: [dayjs().subtract(29, 'days'), dayjs()] },
+        { label: 'This Month', value: [dayjs().startOf('month'), dayjs().endOf('month')] },
+        { label: 'Last Month', value: [dayjs().subtract(1, 'month').startOf('month'), dayjs().subtract(1, 'month').endOf('month')] },
       ];
+     
     
       onOptionSelected(optionValue: any) {
         console.log('Selected Option Value:', optionValue);
