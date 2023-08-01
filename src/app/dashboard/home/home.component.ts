3b9428108be9bc3ac2a6eafc9e13d32424bbdb7f
phillipnzau <nzauonline@gmail.com>
2023-08-01 12:28:08 +0300

    charts init

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemCardComponent } from "../shared/components/item-card/item-card.component";
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [CommonModule, ItemCardComponent]
+    imports: [CommonModule, ItemCardComponent, ChartModule]
 })
 export class HomeComponent {
+
+    chart = new Chart({
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: 'Linechart'
+        },
+        credits: {
+          enabled: false
+        },
+        xAxis: {
+            categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
+          },
+          yAxis: {
+            title: {
+              text: 'Value',
+            },
+          },
+        series: [
+          {
+            name: 'Data',
+            data: [10, 5, 15, 8, 12],
+          } as any
+        ]
+      });
 
 }
