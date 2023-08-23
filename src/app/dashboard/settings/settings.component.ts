a86f39e16dc8e890ea7830254fb149aee1fe07e1
phillipnzau <nzauonline@gmail.com>
2023-08-23 11:48:26 +0300

    change settings view by loged in user

--- src/app/dashboard/settings/settings.component.ts
+++ src/app/dashboard/settings/settings.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { UserEntityService } from '../shared/ngrx-store/user/user-entity.service';
+import { UserModel } from '../shared/models/userModel';
 
 @Component({
   selector: 'app-settings',
@@ -9,6 +11,27 @@ import { RouterModule } from '@angular/router';
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
+  userType: string = '';
+  
+  constructor(private userService: UserEntityService){}
+
+
+  ngOnInit(): void {
+    this.getUserType()
+  }
+
+  // Get the type of user logged in
+  getUserType() {
+    this.userService.entities$.subscribe({
+      next:(data:UserModel[])=>{
+        this.userType = data[0]?.customerId.entityType        
+      },
+      error:(err)=>{
+        console.log('Error getting users entities', err);
+        
+      }
+    })
+  }
 
 }
